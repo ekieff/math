e15f553c14dddd890153160b7617232b85150496
ekieff <kiefferelaine@gmail.com>
2021-08-11 23:45:13 -0500

    transition to while loop

--- src/problem.js
+++ src/problem.js
@@ -1,6 +1,8 @@
 console.log('Hello World')
 function problem(){
-    var num = 13
+    var num = 12
+    while (num > 1)
+{
     if (num%2 ==0){
         console.log('It is an even number')
         num= num/2
@@ -11,5 +13,6 @@ function problem(){
         num = 3*num +1
         console.log(num)
     }
+} 
 }
 problem()
