4d92be69ad842dfa2efafaf784ced867ad355992
ekieff <kiefferelaine@gmail.com>
2021-08-11 23:39:22 -0500

    add basic problem outline

---
+++ src/problem.js
@@ -0,0 +1,15 @@
+console.log('Hello World')
+function problem(){
+    var num = 13
+    if (num%2 ==0){
+        console.log('It is an even number')
+        num= num/2
+        console.log(num)
+    }
+    else{
+        console.log('it is an odd number')
+        num = 3*num +1
+        console.log(num)
+    }
+}
+problem()
