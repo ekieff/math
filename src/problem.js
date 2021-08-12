3625b6828eb6207f6ce6414008f42cac61fc519f
ekieff <kiefferelaine@gmail.com>
2021-08-11 23:51:47 -0500

    add round counter

--- src/problem.js
+++ src/problem.js
@@ -1,18 +1,20 @@
 console.log('Hello World')
 function problem(){
     var num = 12
-    while (num > 1)
-{
-    if (num%2 ==0){
-        console.log('It is an even number')
-        num= num/2
-        console.log(num)
-    }
-    else{
-        console.log('it is an odd number')
-        num = 3*num +1
-        console.log(num)
-    }
-} 
+    var rounds = 0
+    while (num > 1){
+        if (num%2 ==0){
+            console.log('It is an even number')
+            num= num/2
+            rounds += 1
+            console.log('the new number is ' +num +' and the round is ' +rounds)
+        }
+        else{
+            console.log('it is an odd number')
+            num = 3*num +1
+            rounds +=1
+            console.log('the new number is ' +num +' and the round is ' +rounds)
+        }
+    } 
 }
 problem()
