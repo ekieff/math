37145cf2ba7dc8416cc26feb8ba2fc9b371843bb
ekieff <kiefferelaine@gmail.com>
2021-08-17 00:02:32 -0500

    set state for variable

--- src/problem.js
+++ src/problem.js
@@ -1,11 +1,13 @@
 
-import React from "react";
+import React, {useState} from "react";
 
 export default function Problem() {
-  function greetUser(e) {
+var [number, setNum] = useState()
+  function submitNumber(e) {
+      console.log(number)
       e.preventDefault();
-      var num = 40
       var rounds = 0
+      let num = number
       while (num > 1){
                 if (num%2===0){
                     num= num/2
@@ -16,7 +18,14 @@ export default function Problem() {
                     rounds +=1
                 }
             } 
+            console.log(num)
         console.log('The number is now 1 and it took ' +rounds +' rounds to get there.')
+  }
+  function tryIt(e){
+      console.log(e.target.value)
+      setNum(e.target.value)
+      console.log(number)
+
   }
 
   return (
@@ -24,29 +33,11 @@ export default function Problem() {
         <form>
             <label>
             Your Number: 
-            <input type="number" name="num" defaultValue=""/>
+            <input type="number" name="num" defaultValue="" onChange={(e)=>{tryIt(e)}}/>
             </label>
-            <button type="submit" value="Submit" name="submit" onClick={(e)=>{greetUser(e)}}>Submit</button>
+            <button type="submit" value="Submit" name="submit" onClick={(e)=>{submitNumber(e)}}>Submit</button>
         </form>
     </div>
     
   );
 }
-
-
-// function Problem(e){
-//     e.preventDefault();
-//     var num = 12
-//     var rounds = 0
-//     while (num > 1){
-//         if (num%2===0){
-//             num= num/2
-//             rounds += 1
-//         }
-//         else{
-//             num = 3*num +1
-//             rounds +=1
-//         }
-//     } 
-//     console.log('The number is now 1 and it took ' +rounds +' rounds to get there.')
-// }
